f4273fb8240bd6128a725c30e02cdedfe40026d1
mahammadnur <135527996+mahammadnur@users.noreply.github.com>
2023-06-04 20:21:37 +0530

    Add files via upload

---
+++ 5divides.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main (){
+    int number;
+    scanf("%d", &number);
+        if(number % 5){
+
+
+        
+        printf("%d is not divisible by 5",number);
+        }
+        else{
+            printf("%d divisible by 5 ", number);
+        }
+    return 0;
+}
