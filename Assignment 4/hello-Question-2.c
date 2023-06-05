36a526ac35c7520c163dde1ee9800790a887323f
Nur Mahammad <tatian2026@gmail.com>
2023-06-06 00:48:41 +0530

    Add files via upload

---
+++ Assignment 4/hello-Question-2.c
@@ -0,0 +1,8 @@
+/*2. Write a C Program to print Hello on the first line and Students in the second line.*/
+
+#include<stdio.h>
+int main(){
+    printf("hello\nStudents");
+
+    return 0;
+}
---
+++ Assignment 4/teacher'sDay -question4.c
@@ -0,0 +1,8 @@
+/* Write a C program to print “Teacher’s Day” on the screen */
+#include<stdio.h>
+int main(){
+
+printf("Teacher’s Day");
+
+    return 0;
+}
