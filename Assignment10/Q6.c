5322f3ce644e8086201579b1a58b3f6718e031c1
Nur Mahammad <tatian2026@gmail.com>
2023-07-31 19:47:25 +0530

    Q6Update.c

--- Assignment10/Q6.c
+++ Assignment10/Q6.c
@@ -4,7 +4,8 @@
 int main(){
 int n;
     scanf("%d",&n);
-    for(int i = 1; i<=n;i++)
+    for(int i = 1; i<=n;i++) 
+        // for(int i = 1;i <=n;i= i+2;)
     {
         if(i % 2 !=0)
         {
